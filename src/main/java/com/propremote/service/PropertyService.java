e16ea83353b7eb30827904f1f4ff63cec4995659
Diogo Paraíso <diogo@diogoparaiso.com>
2018-12-13 20:03:35 -0300

    Interfaces have only public methods

--- src/main/java/com/propremote/service/PropertyService.java
+++ src/main/java/com/propremote/service/PropertyService.java
@@ -4,8 +4,8 @@ import com.propremote.model.Property;
 
 public interface PropertyService {
 
-    public void addProperty(Property property);
-    public Property getProperty(Long id);
-    public void updateProperty(Property property);
-    public void removeProperty(Long id);
+    void addProperty(Property property);
+    Property getProperty(Long id);
+    void updateProperty(Property property);
+    void removeProperty(Long id);
 }
