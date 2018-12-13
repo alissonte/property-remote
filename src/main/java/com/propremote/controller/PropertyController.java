07419bfa9afda8e382726d0fabfdc141f13dc5a0
Diogo Paraíso <diogo@diogoparaiso.com>
2018-12-13 17:18:35 -0300

    Adding methods for removal and update

--- src/main/java/com/propremote/controller/PropertyController.java
+++ src/main/java/com/propremote/controller/PropertyController.java
@@ -4,18 +4,15 @@ import com.propremote.model.GroupProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PropertyController {
 
     @RequestMapping(value = "/property", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public GroupProperty getProperty() {
-        return new GroupProperty();
+    public ResponseEntity getProperty() {
+        return new ResponseEntity("OK", HttpStatus.OK);
         //TODO: implement property search
     }
 
@@ -23,5 +20,17 @@ public class PropertyController {
     public ResponseEntity<String> addProperty(@RequestBody GroupProperty groupProperty) {
         return new ResponseEntity<>("OK", HttpStatus.CREATED);
         //TODO: implement property creation
+    }
+
+    @RequestMapping(value = "/property/{id:[\\d]+}", method = RequestMethod.DELETE)
+    public ResponseEntity removeShow(@PathVariable String id) {
+        return new ResponseEntity("Success!", HttpStatus.OK);
+        //TODO: implement property removal
+    }
+
+    @RequestMapping(value = "/property/{id:[\\d]+}", method = RequestMethod.PUT)
+    public ResponseEntity removeShow(@PathVariable GroupProperty groupProperty) {
+        return new ResponseEntity("Success!", HttpStatus.OK);
+        //TODO: implement property update
     }
 }
