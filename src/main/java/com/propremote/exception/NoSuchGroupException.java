cebafa981851f4112dfca47970fc095705b3244c
Diogo Paraíso <diogo@diogoparaiso.com>
2018-12-14 14:57:13 -0300

    - New exception for non-existing group

---
+++ src/main/java/com/propremote/exception/NoSuchGroupException.java
@@ -0,0 +1,8 @@
+package com.propremote.exception;
+
+public class NoSuchGroupException extends RuntimeException {
+
+    public NoSuchGroupException(String message) {
+        super(message);
+    }
+}
