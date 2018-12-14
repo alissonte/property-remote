2bf31c8bd464334376ed37b5037cf382e28c3edc
Alisson Rodrigues <alissonte@gmail.com>
2018-12-14 16:33:16 -0300

    Incluindo a lib jersey

--- src/main/java/com/propremote/service/PropertyService.java
+++ src/main/java/com/propremote/service/PropertyService.java
@@ -3,8 +3,4 @@ package com.propremote.service;
 import com.propremote.model.Property;
 
 public interface PropertyService extends GenericService<Property>{
-    /*void addProperty(Property property);
-    Property getProperty(Long id);
-    void updateProperty(Property property);
-    void removeProperty(Long id);*/
 }
